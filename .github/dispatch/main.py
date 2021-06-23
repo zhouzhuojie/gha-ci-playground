71e73fd0c008504eac079606d5c3c354dd078ed8
zhouzhuojie <zhouzhuojie@gmail.com>
2021-06-23 01:38:10 +0000

    Fix dispatch

--- .github/dispatch/main.py
+++ .github/dispatch/main.py
@@ -2,39 +2,38 @@ from __future__ import annotations
 
 import json
 import os
-from dataclasses import dataclass, asdict
+from dataclasses import asdict, dataclass, field
 
 
 @dataclass
 class PR:
-    labels: list[str]
+    labels: list[str] = field(default_factory=list)
 
 
 @dataclass
 class PlanContext:
-    pr: PR
+    pr: PR = field(default_factory=PR)
 
 
 @dataclass
 class Plan:
-    ctx: PlanContext
-    topics: list[str]
+    ctx: PlanContext = field(default_factory=PlanContext)
+    topics: list[str] = field(default_factory=list)
 
     @classmethod
     def from_env(cls) -> Plan:
-        pr = json.loads(os.getenv('PR', '{}'))
+        envPR = json.loads(os.getenv('PR', '{}'))
         plan = Plan()
-        plan.ctx = PlanContext
-        plan.ctx.pr = pr
+        plan.ctx.pr.labels = envPR.get('labels', [])
         return plan
 
     def output(self) -> str:
         print(f"::set-output name=ctx::{json.dumps(asdict(self.ctx))}")
-        print(f"::set-output name=topics::{json.dumps(asdict(self.topics))}")
+        print(f"::set-output name=topics::{json.dumps(self.topics)}")
 
     def dispatch(self) -> None:
         # simple passthrough to generate the labels
-        self.topics = self.ctx.pr['labels']
+        self.topics = self.ctx.pr.labels
         self.output()
 
 
